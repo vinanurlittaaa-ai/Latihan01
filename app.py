aefb45985b9ba64852d4a207b779c05e9fe5f038
vinanurlittaaa <pubgmghz@gmail.com>
2025-11-21 20:57:23 +0700

    Memperbaiki app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import streamlit as st
 
 pages = [
-    st.Page(page="pages/page1.py", titles="Home", icon="🏠"),
-    st.Page(page="pages/page2.py", titles="Visualisasi Data", icon="📊"),
-    st.Page(page="pages/page3.py", titles="Settings", icon="⚙️")
+    st.Page(page="pages/page1.py", title="Home", icon="🏠"),
+    st.Page(page="pages/page2.py", title="Visualisasi Data", icon="📊"),
+    st.Page(page="pages/page3.py", title="Settings", icon="⚙️")
 ]
 
 pg = st.navigation(
     pages,
-    position="sidenar",
+    position="sidebar",
     expanded=True
 )
 
